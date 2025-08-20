ec1175a22a9bb81f4161eb7e92a9ca344e6ee35b
moreira <kauanm0reiratti@gmail.com>
2025-08-20 21:52:33 +0000

    jogo completo

---
+++ jogo.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+
+int main(){
+ printf("DESAFIO SUPER TRUNFO\n");
+ printf("Aqui você irá criar as suas cartas\n");
+ printf("By: Kauan Moreira \n");
+
+ //variaveis primeira carta
+ char estado1 [10]; 
+ char codigo1 [5]; 
+ char nome1 [10];
+ unsigned long int populacao1; 
+ int Pontosturisticos1;
+ float area1;
+ float pib1;
+ float DensidadePopulacional1;
+ float PibPerCapita1;
+ float Inverso1;
+ float SuperPoder1;
+ int ResultadoPopulação, ResultadoPoder, ResultadoÁrea, ResultadoPib, ResultadoDensidadePopulacional, ResultadoPontosTuristicos, ResultadoPibPerCapita;
+
+ printf(" *** Cadastro 1 Carta *** \n");
+ 
+ printf("Digite o estado: \n");
+ scanf("%s", estado1);
+
+ printf("Digite o código (A01-HO4): \n");
+ scanf("%s", codigo1);
+ 
+ printf("Digite o nome da cidade: \n");
+ scanf("%s", nome1);
+
+ printf("Digite a população: \n");
+ scanf("%lu", &populacao1);
+
+ printf("Digite a quantidade de Pontos Turisticos: \n");
+ scanf("%d", &Pontosturisticos1);
+
+ printf("Digite a Área da cidade: \n");
+ scanf("%f", &area1);
+
+ printf("Digite O PIB: \n");
+ scanf("%f", &pib1);
+
+
+ //calculos de Densidade Populacional e Pib Per Capita
+ DensidadePopulacional1 = (float)(populacao1 / area1);
+ printf("A Densidade Populacional é: %f \n", DensidadePopulacional1);
+ PibPerCapita1 = (float)(pib1 / populacao1);
+ printf("O Pib per Capita é: %f \n", PibPerCapita1);
+
+  //calculos para Super Poder 1
+ Inverso1 = (DensidadePopulacional1 / 1 );
+ SuperPoder1 = (float)(populacao1 + area1 + pib1 + Pontosturisticos1 + PibPerCapita1 + Inverso1 );
+
+
+
+ //variaveis segunda carta
+ char estado2 [10]; 
+ char codigo2 [5]; 
+ char nome2 [10];
+ unsigned long int populacao2; 
+ int Pontosturisticos2;
+ float area2;
+ float pib2;
+ float DensidadePopulacional2;
+ float PibPerCapita2;
+ float Inverso2;
+ float SuperPoder2;
+
+  printf(" *** Cadastro 2 Carta *** \n");
+ 
+ printf("Digite o estado: \n");
+ scanf("%s", estado2);
+
+ printf("Digite o código (A01-HO4): \n");
+ scanf("%s", codigo2);
+ 
+ printf("Digite o nome da cidade: \n");
+ scanf("%s", nome2);
+
+ printf("Digite a população: \n");
+ scanf("%lu", &populacao2);
+
+ printf("Digite a quantidade de Pontos Turisticos: \n");
+ scanf("%d", &Pontosturisticos2);
+
+ printf("Digite a Área da cidade: \n");
+ scanf("%f", &area2);
+
+ printf("Digite O PIB: \n");
+ scanf("%f", &pib2);
+
+ //calculos de Densidade Populacional e Pib Per Capita
+ DensidadePopulacional2 = (float)(populacao2 / area2);
+ printf("A Densidade Populacional é: %f \n", DensidadePopulacional2);
+ PibPerCapita2 = (float)(pib2 / populacao2);
+ printf("O Pib per Capita é: %f \n", PibPerCapita2);
+
+ //calculos para Super Poder 2
+ Inverso2 =(DensidadePopulacional2 / 1 );
+ SuperPoder2 = (float)(populacao2 + area2 + pib2 + Pontosturisticos2 + PibPerCapita2 + Inverso2);
+
+  // CARTA 1 COMPLETA
+ printf(" === PRIMEIRA CARTA === \n");
+ printf("Super Poder: %f\n", SuperPoder1);
+ printf("Estado: %s\n", estado1);
+ printf("Código: %s\n", codigo1);
+ printf("Cidade: %s\n", nome1);
+ printf("População: %lu\n", populacao1);
+ printf("Pontos Turísticos: %d\n", Pontosturisticos1);
+ printf("Área: %f\n", area1);
+ printf("PIB: %f\n", pib1); 
+ printf("Densidade Populacional: %f\n", DensidadePopulacional1); 
+ printf("PIB per Capita: %f\n", PibPerCapita1); 
+ printf(" ====================== \n");
+
+ // CARTA 2 COMPLETA
+ printf(" === SEGUNDA CARTA === \n");
+ printf("Super Poder: %f\n", SuperPoder2);
+ printf("Estado: %s\n", estado2);
+ printf("Código: %s\n", codigo2);
+ printf("Cidade: %s\n", nome2);
+ printf("População: %lu\n", populacao2);
+ printf("Pontos Turísticos: %d\n", Pontosturisticos2);
+ printf("Área: %f\n", area2);
+ printf("PIB: %f\n", pib2);
+ printf("Densidade Populacional: %f\n", DensidadePopulacional2); 
+ printf("PIB per Capita: %f\n", PibPerCapita2);
+ printf(" ====================== \n");
+
+  // COMPARAÇÃO DE CARTAS 
+  int resultado1, resultado2, resultado3;
+ int PrimeiroAtributo, SegundoAtributo, TerceiroAtributo;
+
+ printf("\n ======================================================================================================= \n");
+ printf(" *** Comparação de Cartas *** \n");
+ printf("Aqui você ira escolher 3 atributos para decidir quem sera o vencedor do jogo famosa Melhor de 3!! \n ");
+ printf("Unica Regra básica que você tem que entender é: \n");
+ printf("A Densidade Demográfica,  vence a carta com o menor valor.\n");
+ printf("Boa Sorte e Bom Jogo!!\n");
+ printf("\n ======================================================================================================= \n");
+
+printf("*** Comparativo do Primeiro Atributo *** \n"); 
+printf("Escolha um atributo de comparação para as cidades: %s e %s \n", nome1, nome2);
+printf("1. Super Poder\n");
+printf("2. População\n");
+printf("3. PIB\n");
+printf("4. Área\n");
+printf("5. Densidade Populacional\n");
+printf("6. Pib per Capita\n");
+printf("Escolha: ");
+scanf("%d", &PrimeiroAtributo);
+
+switch (PrimeiroAtributo) {
+    case 1:
+        printf("Você escolheu a opção Super Poder\n");
+        resultado1 = SuperPoder1 > SuperPoder2 ? 1 : 0;
+        break;
+    case 2:
+        printf("Você escolheu a opção População!\n");
+        resultado1 = populacao1 > populacao2 ? 1 : 0;
+        break;
+    case 3:
+        printf("Você escolheu a opção PIB!\n");
+        resultado1 = pib1 > pib2 ? 1 : 0;
+        break;
+    case 4:
+        printf("Você escolheu a opção Área!\n");
+        resultado1 = area1 > area2 ? 1 : 0;
+        break;
+    case 5:
+        printf("Você escolheu a opção Densidade Populacional!\n");
+        resultado1 = DensidadePopulacional1 < DensidadePopulacional2 ? 1 : 0;
+        break;
+    case 6:
+        printf("Você escolheu a opção Pib per Capita!\n");
+        resultado1 = PibPerCapita1 > PibPerCapita2 ? 1 : 0;
+        break;
+    default:
+        printf("Opção Inválida\n");
+        break;
+}
+
+printf("\n*** Comparativo do Segundo Atributo ***\n");
+printf("Escolha um atributo de comparação para as cidades: %s e %s \n", nome1, nome2);
+printf("Obs: escolha um atributo diferente do primeiro.\n");
+printf("1. Super Poder\n");
+printf("2. População\n");
+printf("3. PIB\n");
+printf("4. Área\n");
+printf("5. Densidade Populacional\n");
+printf("6. Pib per Capita\n");
+printf("Escolha: ");
+scanf("%d", &SegundoAtributo);
+
+if (SegundoAtributo == PrimeiroAtributo) {
+    printf("Atributo já escolhido!\n");
+} else {
+    switch (SegundoAtributo) {
+        case 1:
+            printf("Você escolheu a opção Super Poder\n");
+            resultado2 = SuperPoder1 > SuperPoder2 ? 1 : 0;
+            break;
+        case 2:
+            printf("Você escolheu a opção População!\n");
+            resultado2 = populacao1 > populacao2 ? 1 : 0;
+            break;
+        case 3:
+            printf("Você escolheu a opção PIB!\n");
+            resultado2 = pib1 > pib2 ? 1 : 0;
+            break;
+        case 4:
+            printf("Você escolheu a opção Área!\n");
+            resultado2 = area1 > area2 ? 1 : 0;
+            break;
+        case 5:
+            printf("Você escolheu a opção Densidade Populacional!\n");
+            resultado2 = DensidadePopulacional1 < DensidadePopulacional2 ? 1 : 0;
+            break;
+        case 6:
+            printf("Você escolheu a opção Pib per Capita!\n");
+            resultado2 = PibPerCapita1 > PibPerCapita2 ? 1 : 0;
+            break;
+        default:
+            printf("Opção Inválida\n");
+            break;
+    }
+}
+
+printf("\n*** Comparativo do Terceiro Atributo ***\n");
+printf("Escolha um atributo de comparação para as cidades: %s e %s \n", nome1, nome2);
+printf("Obs: escolha um atributo diferente dos outros\n");
+printf("1. Super Poder\n");
+printf("2. População\n");
+printf("3. PIB\n");
+printf("4. Área\n");
+printf("5. Densidade Populacional\n");
+printf("6. Pib per Capita\n");
+printf("Escolha: ");
+scanf("%d", &TerceiroAtributo);
+
+// CORREÇÃO: condição mal formada
+if (TerceiroAtributo == PrimeiroAtributo || TerceiroAtributo == SegundoAtributo) {
+    printf("Atributo já escolhido!\n");
+} else {
+    switch (TerceiroAtributo) {
+        case 1:
+            printf("Você escolheu a opção Super Poder\n");
+            resultado3 = SuperPoder1 > SuperPoder2 ? 1 : 0;
+            break;
+        case 2:
+            printf("Você escolheu a opção População!\n");
+            resultado3 = populacao1 > populacao2 ? 1 : 0;
+            break;
+        case 3:
+            printf("Você escolheu a opção PIB!\n");
+            resultado3 = pib1 > pib2 ? 1 : 0;
+            break;
+        case 4:
+            printf("Você escolheu a opção Área!\n");
+            resultado3 = area1 > area2 ? 1 : 0;
+            break;
+        case 5:
+            printf("Você escolheu a opção Densidade Populacional!\n");
+            resultado3 = DensidadePopulacional1 < DensidadePopulacional2 ? 1 : 0;
+            break;
+        case 6:
+            printf("Você escolheu a opção Pib per Capita!\n");
+            resultado3 = PibPerCapita1 > PibPerCapita2 ? 1 : 0;
+            break;
+        default:
+            printf("Opção Inválida\n");
+            break;
+    }
+}
+ if(resultado1 && resultado2 && resultado3){
+    printf("Parabéns, jogador da Carta 1 venceu!!!\n");
+
+} else if (
+    (resultado1 == resultado2 && resultado3 != resultado1) ||
+    (resultado1 == resultado3 && resultado2 != resultado1) ||
+    (resultado2 == resultado3 && resultado1 != resultado2)
+) {
+   printf("Parabéns, jogador da Carta 1 venceu!!!\n");
+} else {
+    printf("Parabéns, jogador da Carta 2 venceu!!!\n");
+}
+
+
+
+return 0;
+
+}
